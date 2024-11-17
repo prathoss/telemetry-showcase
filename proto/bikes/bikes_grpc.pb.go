948a574225d0d01b07d1c98ef93980fdde5cff73
Michal Bradáč <michal.bradac@icloud.com>
2024-11-17 12:42:30 +0100

    add proto definitions

--- proto/bikes/bikes_grpc.pb.go
+++ proto/bikes/bikes_grpc.pb.go
@@ -8,6 +8,7 @@ package bikes
 
 import (
 	context "context"
+	latlng "google.golang.org/genproto/googleapis/type/latlng"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
@@ -19,7 +20,10 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	Bikes_GetBikeById_FullMethodName = "/showcase.Bikes/GetBikeById"
+	Bikes_GetBikeById_FullMethodName      = "/showcase.Bikes/GetBikeById"
+	Bikes_ListBikes_FullMethodName        = "/showcase.Bikes/ListBikes"
+	Bikes_SetBikeReserved_FullMethodName  = "/showcase.Bikes/SetBikeReserved"
+	Bikes_SetBikeAvailable_FullMethodName = "/showcase.Bikes/SetBikeAvailable"
 )
 
 // BikesClient is the client API for Bikes service.
@@ -27,6 +31,9 @@ const (
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type BikesClient interface {
 	GetBikeById(ctx context.Context, in *GetBikeByIdRequest, opts ...grpc.CallOption) (*BikeReply, error)
+	ListBikes(ctx context.Context, in *latlng.LatLng, opts ...grpc.CallOption) (*ListBikesReply, error)
+	SetBikeReserved(ctx context.Context, in *SetBikeReservedRequest, opts ...grpc.CallOption) (*SetBikeReservedReply, error)
+	SetBikeAvailable(ctx context.Context, in *SetBikeAvailableRequest, opts ...grpc.CallOption) (*SetBikeAvailableReply, error)
 }
 
 type bikesClient struct {
@@ -47,11 +54,44 @@ func (c *bikesClient) GetBikeById(ctx context.Context, in *GetBikeByIdRequest, o
 	return out, nil
 }
 
+func (c *bikesClient) ListBikes(ctx context.Context, in *latlng.LatLng, opts ...grpc.CallOption) (*ListBikesReply, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(ListBikesReply)
+	err := c.cc.Invoke(ctx, Bikes_ListBikes_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *bikesClient) SetBikeReserved(ctx context.Context, in *SetBikeReservedRequest, opts ...grpc.CallOption) (*SetBikeReservedReply, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(SetBikeReservedReply)
+	err := c.cc.Invoke(ctx, Bikes_SetBikeReserved_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *bikesClient) SetBikeAvailable(ctx context.Context, in *SetBikeAvailableRequest, opts ...grpc.CallOption) (*SetBikeAvailableReply, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(SetBikeAvailableReply)
+	err := c.cc.Invoke(ctx, Bikes_SetBikeAvailable_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // BikesServer is the server API for Bikes service.
 // All implementations must embed UnimplementedBikesServer
 // for forward compatibility.
 type BikesServer interface {
 	GetBikeById(context.Context, *GetBikeByIdRequest) (*BikeReply, error)
+	ListBikes(context.Context, *latlng.LatLng) (*ListBikesReply, error)
+	SetBikeReserved(context.Context, *SetBikeReservedRequest) (*SetBikeReservedReply, error)
+	SetBikeAvailable(context.Context, *SetBikeAvailableRequest) (*SetBikeAvailableReply, error)
 	mustEmbedUnimplementedBikesServer()
 }
 
@@ -64,6 +104,15 @@ type UnimplementedBikesServer struct{}
 
 func (UnimplementedBikesServer) GetBikeById(context.Context, *GetBikeByIdRequest) (*BikeReply, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetBikeById not implemented")
+}
+func (UnimplementedBikesServer) ListBikes(context.Context, *latlng.LatLng) (*ListBikesReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ListBikes not implemented")
+}
+func (UnimplementedBikesServer) SetBikeReserved(context.Context, *SetBikeReservedRequest) (*SetBikeReservedReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SetBikeReserved not implemented")
+}
+func (UnimplementedBikesServer) SetBikeAvailable(context.Context, *SetBikeAvailableRequest) (*SetBikeAvailableReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SetBikeAvailable not implemented")
 }
 func (UnimplementedBikesServer) mustEmbedUnimplementedBikesServer() {}
 func (UnimplementedBikesServer) testEmbeddedByValue()               {}
@@ -104,6 +153,60 @@ func _Bikes_GetBikeById_Handler(srv interface{}, ctx context.Context, dec func(i
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Bikes_ListBikes_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(latlng.LatLng)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(BikesServer).ListBikes(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: Bikes_ListBikes_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(BikesServer).ListBikes(ctx, req.(*latlng.LatLng))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Bikes_SetBikeReserved_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(SetBikeReservedRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(BikesServer).SetBikeReserved(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: Bikes_SetBikeReserved_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(BikesServer).SetBikeReserved(ctx, req.(*SetBikeReservedRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Bikes_SetBikeAvailable_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(SetBikeAvailableRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(BikesServer).SetBikeAvailable(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: Bikes_SetBikeAvailable_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(BikesServer).SetBikeAvailable(ctx, req.(*SetBikeAvailableRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Bikes_ServiceDesc is the grpc.ServiceDesc for Bikes service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -114,6 +217,18 @@ var Bikes_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetBikeById",
 			Handler:    _Bikes_GetBikeById_Handler,
+		},
+		{
+			MethodName: "ListBikes",
+			Handler:    _Bikes_ListBikes_Handler,
+		},
+		{
+			MethodName: "SetBikeReserved",
+			Handler:    _Bikes_SetBikeReserved_Handler,
+		},
+		{
+			MethodName: "SetBikeAvailable",
+			Handler:    _Bikes_SetBikeAvailable_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
---
+++ proto/go.mod
@@ -0,0 +1,16 @@
+module github.com/prathoss/telemetry_showcase/proto
+
+go 1.22.6
+
+require (
+	google.golang.org/genproto v0.0.0-20240823204242-4ba0660f739c
+	google.golang.org/grpc v1.66.0
+	google.golang.org/protobuf v1.34.2
+)
+
+require (
+	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240823204242-4ba0660f739c // indirect
+)
